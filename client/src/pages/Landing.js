129e478bddf53fc3bbae0c6f73874bb7da416f3b
Binny <ibinstock@outlook.com>
2022-12-13 08:29:01 -0800

    got some of the homepage styling done

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './Landing.css';
+
 const Landing = () => {
   return (
-    <main>
-      <Link to='/signup' style={{ textDecoration: 'none' }}>
-        <h1>Sign Up now</h1>
-      </Link>
-      <Link to='/login' style={{ textDecoration: 'none' }}>
-        <h1>Login now</h1>
-      </Link>
-    </main>
+    <div className="landing-bg">
+      <div className="landing-border">
+        <Link to='/signup' style={{ textDecoration: 'none' }}>
+          <h1>Sign Up now</h1>
+        </Link>
+        <Link to='/login' style={{ textDecoration: 'none' }}>
+          <h1>Login now</h1>
+        </Link>
+      </div>
+    </div>
   )
 };
 
