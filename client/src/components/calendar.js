782a3d65242ac34963112ef3c8d80cdae30a7246
Binny <ibinstock@outlook.com>
2022-12-16 12:13:07 -0800

    added lotta styling, still not perfect

--- client/src/components/calendar.js
+++ client/src/components/calendar.js
@@ -24,38 +24,128 @@ const localizer = dateFnsLocalizer({
 });
 
 const events = [
-    {
-        title: "Project 3",
-        allDay: true,
-        start: "Mon Dec 19 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-        end: "Mon Dec 20 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-    },
-    {
-        title: "Vacation",
-        start: new Date(2022, 11, 20),
-        end: new Date(2022, 11, 26),
-    },
-    {
-        title: "Graduation",
-        start: new Date(2022, 11, 19),
-        end: new Date(2022, 11, 19),
-    },
+  {
+    title: "Project 3",
+    allDay: true,
+    start: "Mon Dec 19 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
+    end: "Mon Dec 20 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
+  },
+  {
+    title: "Vacation",
+    start: new Date(2022, 11, 20),
+    end: new Date(2022, 11, 26),
+  },
+  {
+    title: "Graduation",
+    start: new Date(2022, 11, 19),
+    end: new Date(2022, 11, 19),
+  },
+  {
+    title: "Graduation2",
+    start: new Date(2022, 11, 19),
+    end: new Date(2022, 11, 19),
+  },
+  {
+    title: "Graduation3",
+    start: new Date(2022, 11, 19),
+    end: new Date(2022, 11, 19),
+  },
+  {
+    title: "Graduation4",
+    start: new Date(2022, 11, 19),
+    end: new Date(2022, 11, 19),
+  },
+  {
+    title: "Graduation5",
+    start: new Date(2022, 11, 19),
+    end: new Date(2022, 11, 19),
+  },
+  {
+    title: "Graduation6",
+    start: new Date(2022, 11, 19),
+    end: new Date(2022, 11, 19),
+  },
+  {
+    title: "Graduation7",
+    start: new Date(2022, 11, 19),
+    end: new Date(2022, 11, 19),
+  }
 ];
 
+// Card template html
+function card_temp(title = "") {
+  return `
+  <div class="card">
+    <h3>${title}</h3>
+    <div class="card_inner"></div>
+  </div>
+  `;
+};
 
+// When the user clicks on the button, toggle between hiding and showing the dropdown content
+window.onclick = function(event) {
+  if (event.target.matches('.plusBtn')) {
+    document.querySelector('.eventDropdown').classList.toggle("show");
+  } else if (event.target.matches('.rbc-event-content') && event.target.innerText === "Vacation") {
+    let copy = document.querySelector('[title="Vacation"]');
+    copy.insertAdjacentHTML('afterend', card_temp("Vacation"));
+    document.querySelector('.card').style.width = `${copy.offsetWidth}px`;
+  } else {
+    // document.querySelector('.card').style.display = "none";
+  }
+}
 
- function CalendarComp() {
-    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
-    const [allEvents, setAllEvents] = useState(events);
-    console.log(allEvents)
-    function handleAddEvent() {
-        setAllEvents([...allEvents, newEvent])
-      }
-      
+function CalendarComp() {
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  const [allEvents, setAllEvents] = useState(events);
+  console.log(allEvents)
+  function handleAddEvent() {
+    setAllEvents([...allEvents, newEvent])
+  }
  
   return (
     <>
-    <div className="quick_style">
+    <div className="custom_panels">
+      <button onClick={AuthService.logout}>Logout</button>
+      <button className="plusBtn">+</button>
+      <div className="eventDropdown">
+        <div className="row">
+          <h3>Add Event</h3>
+          <input 
+            type='text' 
+            placeholder=''
+            value={newEvent.title} 
+            onChange={(e) => setNewEvent({...newEvent, title: e.target.value})}
+          />
+        </div>
+        <div className="col">
+          <div className="row">
+            <h3>Start Date</h3>
+            <DatePicker 
+              placeholderText=''
+              selected={newEvent.start} 
+              onChange={(start) => setNewEvent({...newEvent, start})}
+            />
+          </div>
+          <div className="row">
+            <h3>End Date</h3>
+            <DatePicker 
+              placeholderText='' 
+              selected={newEvent.end} 
+              onChange={(end) => setNewEvent({...newEvent, end})}
+            />
+          </div>
+        </div>
+        <button 
+          type='text' 
+          placeholder='Add Event' 
+          onClick={handleAddEvent}
+          style={{padding: '10px 10px 5px 10px'}}
+        >Add Event</button>
+        <button className="plusBtn">-</button>
+      </div>
+    </div>
+    {/* <div className="quick_style">
       <button onClick={AuthService.logout}>Logout</button>
       <h1>Crisp</h1>
       <h2>Add New Event</h2>
@@ -71,7 +161,7 @@ const events = [
         />
         <button type='text' placeholder='Add Event' style={{marginTop: '10px', height: '20px', width: '5%'}} onClick={handleAddEvent}></button>
       </div>
-    </div>
+    </div> */}
     <Calendar
         localizer={localizer}
         startAccessor="start"
@@ -81,6 +171,6 @@ const events = [
       />
     </>
   );
-  
 }
+
 export default CalendarComp;
